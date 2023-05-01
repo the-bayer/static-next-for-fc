45866dcaec53d77f018b4317b08f2da84328e82f
Blake Bayer <94391693+the-bayer@users.noreply.github.com>
2023-05-01 08:04:24 -0400

    V8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            V7
+            V8
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
